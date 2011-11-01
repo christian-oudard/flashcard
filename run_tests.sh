a24aa265abbd1673d4c3e6ddc9a25cd917c4c04a
Christian Oudard <christian.oudard@gmail.com>
2011-11-01 19:03:34 -0400

    Updated test running script to cover tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
 nosetests --with-coverage \
-	--cover-package flashcard \
-	--cover-package tests \
+    --cover-package=flashcard \
+    --cover-package=tests \
+    --cover-tests \
+    $@
